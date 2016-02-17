f698722a3c7943719aa933070aadcb82e0a96964
Alaanzr <Alaanzr@gmail.com>
2016-02-17 15:54:03 +0000

    Average price function working. Removed redundant karma test file

--- app/controllers/price.js
+++ app/controllers/price.js
@@ -1,7 +1,17 @@
-var api = require('./apiCaller.js');
+var api = require('./apiCaller.js'),
+  priceData = {};
 
 exports.request = function(req, res) {
   api(function(body) {
-    res.json(body);
+    priceData.averagePrice = averagePrice(body);
+    res.json(priceData);
   });
+};
+
+averagePrice = function(data) {
+  var total = 0;
+  for (var i = 0; i < data.length; i++) {
+    total += parseInt(data[i].price);
+  }
+  return total / data.length;
 };
--- test/price.spec.js
+++ test/price.spec.js
@@ -1,6 +1,12 @@
 describe('PriceCtrl', function() {
 
-  var price = require('../app/controllers/price.js');
+  var priceCtrl = require('../app/controllers/price.js');
   var mockData = require('./mockData.js');
+
+  describe('averagePrice function', function() {
+    it('returns the average price for listed items', function() {
+      expect(averagePrice(mockData)).toEqual(84);
+    });
+  });
 
 });
