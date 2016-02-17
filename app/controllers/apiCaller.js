423bfd04fc6616e13b083f0cf12ca33b17b8d975
Alaanzr <Alaanzr@gmail.com>
2016-02-17 23:01:41 +0000

    Re-added necessary code to apiCaller.js

--- app/controllers/apiCaller.js
+++ app/controllers/apiCaller.js
@@ -3,7 +3,7 @@ var request = require('request'), url = 'https://openapi.etsy.com/v2/listings/ac
 module.exports = function(callback) {
   request(url, function(err, res, body) {
     if (!err && res.statusCode === 200) {
-      callback();
+      callback(JSON.parse(body).results);
     } else {
       console.log(err);
     }
